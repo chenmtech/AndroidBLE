e6c19fd1331c0693a0be9b21e9f5bd0874f7b299
chenmtech <chenm91@163.com>
2019-06-20 06:58:12 +0800

    commit;

--- ble/src/main/java/com/cmtech/android/ble/extend/BleGattCommand.java
+++ ble/src/main/java/com/cmtech/android/ble/extend/BleGattCommand.java
@@ -6,29 +6,49 @@ import com.cmtech.android.ble.core.BluetoothGattChannel;
 import com.cmtech.android.ble.core.DeviceMirror;
 import com.cmtech.android.ble.utils.HexUtil;
 
+
 /**
- * BleGattCommand: 表示要执行的一条Gatt操作命令
- * Created by bme on 2018/3/1.
+  *
+  * ClassName:      BleGattCommand
+  * Description:    表示一条Gatt操作命令
+  * Author:         chenm
+  * CreateDate:     2018-03-01 06:42
+  * UpdateUser:     chenm
+  * UpdateDate:     2019-06-20 06:42
+  * UpdateRemark:   更新说明
+  * Version:        1.0
  */
 
 class BleGattCommand{
     private final DeviceMirror deviceMirror;          // 执行命令的设备镜像
+
     private final BluetoothGattChannel channel;       // 执行命令的通道
+
     private final IBleCallback dataOpCallback;        // 数据操作回调
+
     private final byte[] writtenData;                 // 如果是写操作，存放要写的数据；如果是notify或indicate操作，存放enable数据
+
     private final IBleCallback notifyOpCallback;      // 如果是notify或indicate操作，存放notify或indicate的回调
+
     private final String elementDescription; // 命令操作的element的描述符
+
     private final boolean isInstantCommand; // 是否是立刻执行的命令，即不需要通过蓝牙通信的命令
 
     private BleGattCommand(DeviceMirror deviceMirror, BluetoothGattChannel channel,
                            IBleCallback dataOpCallback,
                            byte[] writtenData, IBleCallback notifyOpCallback, String elementDescription, boolean isInstantCommand) {
         this.deviceMirror = deviceMirror;
+
         this.channel = channel;
+
         this.dataOpCallback = dataOpCallback;
+
         this.writtenData = writtenData;
+
         this.notifyOpCallback = notifyOpCallback;
+
         this.elementDescription = elementDescription;
+
         this.isInstantCommand = isInstantCommand;
     }
 
@@ -51,6 +71,7 @@ class BleGattCommand{
     boolean execute() {
         if(isInstantCommand && dataOpCallback != null) {
             dataOpCallback.onSuccess(null, null, null);
+
             return true;
         }
 
@@ -59,32 +80,37 @@ class BleGattCommand{
         switch (channel.getPropertyType()) {
             case PROPERTY_READ:
                 deviceMirror.bindChannel( dataOpCallback, channel);
+
                 deviceMirror.readData();
                 break;
+
             case PROPERTY_WRITE:
                 deviceMirror.bindChannel( dataOpCallback, channel);
+
                 deviceMirror.writeData(writtenData);
                 break;
+
             case PROPERTY_NOTIFY:
-                deviceMirror.bindChannel( dataOpCallback, channel);
-                if(writtenData[0] == 1) {
-                    deviceMirror.registerNotify(false);
-                    deviceMirror.setNotifyListener(channel.getGattInfoKey(), notifyOpCallback);
-                } else {
-                    deviceMirror.unregisterNotify(false);
-                    deviceMirror.removeReceiveCallback(channel.getGattInfoKey());
-                }
-                break;
             case PROPERTY_INDICATE:
+                boolean isIndication = true;
+
+                if(channel.getPropertyType() == PropertyType.PROPERTY_NOTIFY) {
+                    isIndication = false;
+                }
+
                 deviceMirror.bindChannel( dataOpCallback, channel);
+
                 if(writtenData[0] == 1) {
-                    deviceMirror.registerNotify(true);
+                    deviceMirror.registerNotify(isIndication);
+
                     deviceMirror.setNotifyListener(channel.getGattInfoKey(), notifyOpCallback);
                 } else {
-                    deviceMirror.unregisterNotify(true);
+                    deviceMirror.unregisterNotify(isIndication);
+
                     deviceMirror.removeReceiveCallback(channel.getGattInfoKey());
                 }
                 break;
+
             default:
                 break;
         }
@@ -98,11 +124,14 @@ class BleGattCommand{
         switch (channel.getPropertyType()) {
             case PROPERTY_READ:
                 return channel.getPropertyType()+" "+elementDescription;
+
             case PROPERTY_WRITE:
                 return channel.getPropertyType() + " " + elementDescription + " " + HexUtil.encodeHexStr(writtenData);
+
             case PROPERTY_NOTIFY:
             case PROPERTY_INDICATE:
                 return channel.getPropertyType() + " " + elementDescription + " " + ((writtenData[0] != 0));
+
                 default:
                     return "";
         }
