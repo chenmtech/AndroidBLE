2997fabd99d95ac6de5599e7d86288a97a90db54
chenmtech <1276094137@qq.com>
2019-11-19 18:23:30 +0800

    Revert "commit;"
    This reverts commit ccfa808ac19d5abb955738745764c13ee7308bd5.

--- ble/src/main/java/com/cmtech/android/ble/core/AbstractDevice.java
+++ ble/src/main/java/com/cmtech/android/ble/core/AbstractDevice.java
@@ -1,5 +1,8 @@
 package com.cmtech.android.ble.core;
 
+import android.content.Context;
+
+import com.cmtech.android.ble.R;
 import com.vise.log.ViseLog;
 
 import java.util.LinkedList;
@@ -7,15 +10,25 @@ import java.util.List;
 import java.util.Objects;
 
 import static com.cmtech.android.ble.core.BleDeviceState.CLOSED;
-import static com.cmtech.android.ble.core.BleDeviceState.CONNECT;
 import static com.cmtech.android.ble.core.BleDeviceState.DISCONNECT;
 import static com.cmtech.android.ble.core.BleDeviceState.FAILURE;
+import static com.cmtech.android.ble.core.BleDeviceState.CONNECT;
 import static com.cmtech.android.ble.core.BleDeviceState.SCANNING;
 
-public abstract class AbstractDevice implements IDevice{
+public abstract class AbstractDevice {
+    public static final int INVALID_BATTERY = -1; // 无效电池电量
+    public static final int MSG_INVALID_OPERATION = R.string.invalid_operation;
+
+    // 设备监听器接口
+    public interface OnBleDeviceListener {
+        void onStateUpdated(final AbstractDevice device); // 设备状态更新
+        void onExceptionMsgNotified(final AbstractDevice device, int msgId); // 异常消息通知
+        void onBatteryUpdated(final AbstractDevice device); // 电池电量更新
+    }
+
     protected final DeviceRegisterInfo registerInfo; // 注册信息
     protected volatile BleDeviceState state = CLOSED; // 实时状态
-    private final List<OnDeviceListener> listeners; // 监听器列表
+    private final List<OnBleDeviceListener> listeners; // 监听器列表
     private int battery = INVALID_BATTERY; // 电池电量
 
 
@@ -27,48 +40,37 @@ public abstract class AbstractDevice implements IDevice{
         listeners = new LinkedList<>();
     }
 
-    @Override
     public DeviceRegisterInfo getRegisterInfo() {
         return registerInfo;
     }
-    @Override
     public void updateRegisterInfo(DeviceRegisterInfo registerInfo) {
         this.registerInfo.update(registerInfo);
     }
-    @Override
     public String getAddress() {
         return registerInfo.getMacAddress();
     }
-    @Override
     public String getName() {
         return registerInfo.getName();
     }
-    @Override
     public String getUuidString() {
         return registerInfo.getUuidStr();
     }
-    @Override
     public String getImagePath() {
         return registerInfo.getImagePath();
     }
-    @Override
     public BleDeviceState getState() {
         return state;
     }
-    @Override
-    public boolean isScanning() {
+    protected boolean isScanning() {
         return state == SCANNING;
     }
-    @Override
-    public boolean isConnected() {
+    protected boolean isConnected() {
         return state == CONNECT;
     }
-    @Override
-    public boolean isDisconnected() {
+    protected boolean isDisconnected() {
         return state == FAILURE || state == DISCONNECT;
     }
-    @Override
-    public void setState(BleDeviceState state) {
+    protected void setState(BleDeviceState state) {
         if(this.state != state) {
             ViseLog.e("The state of device " + getAddress() + " is " + state);
             this.state = state;
@@ -76,39 +78,46 @@ public abstract class AbstractDevice implements IDevice{
         }
     }
     // 更新设备状态
-    @Override
     public void updateState() {
-        for(OnDeviceListener listener : listeners) {
+        for(OnBleDeviceListener listener : listeners) {
             if(listener != null) {
                 listener.onStateUpdated(this);
             }
         }
     }
-    @Override
     public int getBattery() {
         return battery;
     }
-    @Override
-    public void setBattery(final int battery) {
+    protected void setBattery(final int battery) {
         if(this.battery != battery) {
             this.battery = battery;
             updateBattery();
         }
     }
-    @Override
-    public final void addListener(OnDeviceListener listener) {
+    public final void addListener(OnBleDeviceListener listener) {
         if(!listeners.contains(listener)) {
             listeners.add(listener);
         }
     }
-    @Override
-    public final void removeListener(OnDeviceListener listener) {
+    public final void removeListener(OnBleDeviceListener listener) {
         listeners.remove(listener);
     }
 
+    public abstract void open(Context context); // 打开设备
+    public abstract void switchState(); // 切换状态
+    public abstract void callDisconnect(boolean stopAutoScan); // 请求断开
+    public abstract boolean isStopped(); // 是否停止
+    public abstract void close(); // 关闭设备
+    public abstract void clear(); // 清除设备
+
+
+    protected abstract boolean executeAfterConnectSuccess(); // 连接成功后执行的操作
+    protected abstract void executeAfterConnectFailure(); // 连接错误后执行的操作
+    protected abstract void executeAfterDisconnect(); // 断开连接后执行的操作
+
     // 更新电池电量
     private void updateBattery() {
-        for (final OnDeviceListener listener : listeners) {
+        for (final OnBleDeviceListener listener : listeners) {
             if (listener != null) {
                 listener.onBatteryUpdated(this);
             }
@@ -117,7 +126,7 @@ public abstract class AbstractDevice implements IDevice{
 
     // 通知异常消息
     protected void notifyExceptionMessage(int msgId) {
-        for(OnDeviceListener listener : listeners) {
+        for(OnBleDeviceListener listener : listeners) {
             if(listener != null) {
                 listener.onExceptionMsgNotified(this, msgId);
             }
